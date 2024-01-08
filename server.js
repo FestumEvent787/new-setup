179431cd4a3887ae4261843ee91823291e45981c
ronakDobariya <fe.backend0001@gmail.com>
2024-01-08 17:14:57 +0530

    fix change

---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config({ path: ".env" });
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import db from "./db";
+import { failAction } from "./utils/response";
+import http from "http";
+import helmet from "helmet";
+
+import routes from "./api";
+
+const app = express();
+const port = process.env.PORT ? process.env.PORT : 2000;
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Access-Control-Allow-Origin
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+
+app.use("/", (req, res) =>
+  res.json({ message: "Hello Server Now Working......." })
+);
+// app.use("/api", routes);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
