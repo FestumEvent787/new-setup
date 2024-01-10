d8b40b83390cfb639da6dc205c7ee46486862842
ronakDobariya <fe.backend0001@gmail.com>
2024-01-10 09:59:49 +0530

    chnage new

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-require("rootpath")();
-require("dotenv").config({ path: "./.env" });
-require("./server.js");
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ? process.env.PORT : 2000;
+
+app.use("/", (req, res) => res.json("server working now......."));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
--- server.js
+++ server.js
@@ -1,36 +1,34 @@
 import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import path from "path";
-import db from "./db";
-import { failAction } from "./utils/response";
-require("dotenv").config({ path: ".env" });
-import http from "http";
-import helmet from "helmet";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import path from "path";
+// import db from "./db";
+// import { failAction } from "./utils/response";
+// require("dotenv").config({ path: ".env" });
+// import http from "http";
+// import helmet from "helmet";
 
-import routes from "./api";
+// import routes from "./api";
 
 const app = express();
 const port = process.env.PORT ? process.env.PORT : 2000;
 
-app.use(helmet());
+// app.use(helmet());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Access-Control-Allow-Origin
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     credentials: true,
+//   })
+// );
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/api/v1/role/details", (req, res) =>
-  res.json("server working.......")
-);
+// app.use(express.static(path.join(__dirname, "public")));
+app.use("/", (req, res) => res.json("server working......."));
 // app.use("/api", routes);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
